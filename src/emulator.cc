3fa7ff65b1abe1dbae782f3fa4540bd8612921bd
Zackary Ayoun <ayoun_z@epita.fr>
2014-07-02 22:09:11 +0200

    timers done

--- src/emulator.cc
+++ src/emulator.cc
@@ -23,10 +23,24 @@ Emulator::~Emulator()
 
 int Emulator::run()
 {
+    uint32_t last = SDL_GetTicks();
     while (!state_)
     {
         if (kb_.is_key_pressed(SDLK_ESCAPE))
             break;
+        if (cpu_.timer_get() > 0)
+            cpu_.timer_set(cpu_.timer_get() - 1);
+        if (cpu_.sound_get() > 0)
+        {
+            std::cout << '\a' << std::endl;
+            cpu_.sound_set(cpu_.sound_get() - 1);
+        }
+
+        // Fetch, Decode, Execute here!
+
+        while (SDL_GetTicks() - last < 16)
+            continue;
+        last = SDL_GetTicks();
     }
     SDL_Quit();
     return 0;
